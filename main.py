a8391fe90888e5d28feadc3e5a42a8334709ab11
navid-shirmohammadi <47769554+navid-shirmohammadi@users.noreply.github.com>
2019-03-06 16:46:25 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,166 +1,203 @@
 import cv2
-import numpy as np
+from cv2 import aruco
+import draw
+from numpy import sin, cos, pi
+import scipy
+
 from timeit import default_timer as timer
 from datetime import datetime
 
-import draw
+
+import serial
 
 '''
 --------------------------------
-           setting
+		variables
 --------------------------------
 '''
-# setting range of blue color in hsv format
-lower_blue = np.array([110, 50, 50])
-upper_blue = np.array([130, 255, 255])
-# setting range of purple color in hsv format
-lower_purple = np.array([140, 50, 50])
-upper_purple = np.array([160, 255, 255])
-
 # the minimum distance of fish from target
 reach_target_distance = 10
 
-path = []  # path user selected for robot in editor
-cap = cv2.VideoCapture(0)  # index of camera we use
-kernel = np.ones((9, 9), np.uint8)  # kernel size for removing noise using morphologyEx
+# select camera
+cap = cv2.VideoCapture(0)
+
+'''
+--------------------------------
+		   init
+--------------------------------
+'''
+ser = serial.Serial('COM4', 9600, timeout=0, rtscts=1)
+
+# load aruco dict
+aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
+parameters = aruco.DetectorParameters_create()
+
+# I used this vars for control function
+amplitude = 2
+direction = 95
+bias_angle = 20
+freq = 2 * 2 * pi
+
+# path user selected for robot in editor
+path = []
 
 # data we report
-data = {'start time': datetime.now(),
-        'position': []}
+data = {'start time': datetime.now(), 'position': []}
 
-start = timer()  # start timer
+# start timer
+start = timer()
 
 '''
 --------------------------------
-           functions
+		   functions
 --------------------------------
 '''
 
 
-def editor(frame):
-    """ this function get first frame of camera and let the user draw path for robot to go """
-    global path
-    path = draw.run(frame)
+def editor(image):
+	""" this function get first frame of camera and let the user draw path for robot to go """
+	global path
+	path = draw.run(image)
 
 
 def camera():
-    """ this function handle all camera related operations on one frame and return frame and time """
-    if cap.isOpened():
-        _, frame = cap.read()  # read a frame
-        frame = cv2.flip(frame, 1)  # flip the frame vertically
-        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)  # change color space from BGR to HSV
+	""" this function handle all camera related operations on one frame and return frame and time """
+	if cap.isOpened():
+		_, frame = cap.read()  # read a frame
+		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+		cdf = cv2.equalizeHist(gray)
+		corners, ids, rejectedImgPoints = aruco.detectMarkers(cdf, aruco_dict, parameters=parameters)
 
-        # define masks
-        blue_mask = cv2.inRange(hsv, lower_blue, upper_blue)
-        purple_mask = cv2.inRange(hsv, lower_purple, upper_purple)
+		for corner in corners:
+			coord1 = (int(corner[0, 0, 0]), int(corner[0, 0, 1]))
+			coord2 = (int(corner[0, 1, 0]), int(corner[0, 1, 1]))
+			coord3 = (int(corner[0, 2, 0]), int(corner[0, 2, 1]))
+			coord4 = (int(corner[0, 3, 0]), int(corner[0, 3, 1]))
+			cv2.line(frame, coord1, coord2, (0, 0, 255), 3)
+			cv2.line(frame, coord1, coord4, (255, 0, 0), 3)
 
-        # filter noise
-        opening_blue = cv2.morphologyEx(blue_mask, cv2.MORPH_OPEN, kernel)
-        opening_purple = cv2.morphologyEx(purple_mask, cv2.MORPH_OPEN, kernel)
-
-        # thresh hold the image
-        _, thresh_blue = cv2.threshold(opening_blue, 127, 255, 0)
-        _, thresh_purple = cv2.threshold(opening_purple, 127, 255, 0)
-
-        # get contours
-        contours_blue, _ = cv2.findContours(thresh_blue, 1, 2)
-        contours_purple, _ = cv2.findContours(thresh_purple, 1, 2)
-
-        # draw contours
-        try:
-            (x_blue, y_blue), radius_blue = cv2.minEnclosingCircle(contours_blue[0])
-            (x_purple, y_purple), radius_purple = cv2.minEnclosingCircle(contours_purple[0])
-            center_blue = (int(x_blue), int(y_blue))
-            center_purple = (int(x_purple), int(y_purple))
-
-            frame = cv2.circle(frame, center_blue, int(radius_blue), (0, 0, 255), 2)
-            frame = cv2.circle(frame, center_purple, int(radius_purple), (0, 0, 255), 2)
-            return frame, timer() - start, center_blue, center_purple
-
-        except IndexError:
-            # if not found position of both circles return -1,-1 as center of them
-            return frame, timer() - start, (-1, -1), (-1, -1)
+		return frame, timer() - start, corners
 
 
 def report():
-    # report data
-    with open('data.txt', 'a') as file:
-        file.write('\n=========================\n')
-        file.write(str(data['start time']))
-        file.write('\n=========================\n')
+	# report data
+	with open('data.txt', 'a') as file:
+		file.write('\n=========================\n')
+		file.write(str(data['start time']))
+		file.write('\n=========================\n')
 
-        for item in data['position']:
-            file.write(str(item) + '\n')
-        file.close()
+		for item in data['position']:
+			file.write(str(item) + '\n')
+		file.close()
 
+angle_backup = 0
 
-def control_unit(blue_center, purple_center, target):
-    pass
+def control(fish_corners, target_):
+	global ser, amplitude, direction, freq, bias_angle, angle_backup
+
+	t = timer() - start
+
+	angle = angle_backup
+
+	if fish_corners:
+		fish_corners = fish_corners[0][0]
+
+		pt0 = [(fish_corners[0][0]+fish_corners[2][0])/2,
+			   (fish_corners[0][1]+fish_corners[2][1])/2]
+		pt1 = target_
+
+		# print(pt0)
+
+		direction = (scipy.angle((pt0[0]-pt1[0])+(pt0[1]-pt1[1])*1j, True) - 90) % 360
+
+		angle = (direction + bias_angle * sin(freq * t))
+		angle_backup = angle
+		print(direction)
+
+	x = amplitude * cos(int(angle) * 3.1415 / 180)
+	y = amplitude * sin(int(angle) * 3.1415 / 180)
+
+	poly_x = 114.8 * abs(x) ** 0.5157
+	poly_y = 114.8 * abs(y) ** 0.5157
+
+	if x < 0:
+		poly_x = -poly_x
+	if y < 0:
+		poly_y = -poly_y
+
+	COMMAND = str(int(poly_x)) + '^' + str(int(poly_y)) + '!'
+
+	ser.write(bytes(COMMAND, 'ascii'))
 
 
 '''
 --------------------------------
-             main
+			 main
 --------------------------------
 '''
 
 if __name__ == '__main__':
-    # enter editor
-    frame, _, _, _ = camera()  # just get a frame for editor
-    editor(frame)
+	# enter editor
+	frame, _, _ = camera()  # just get a frame for editor
+	editor(frame)
 
-    target = 0  # index of target robot has to reach
+	target = 0  # index of target robot has to reach
 
-    FPS = []  # use this list to calculate frame per second of camera
+	# FPS = []  # use this list to calculate frame per second of camera
 
-    while True:
-        frame, time, c_blue, c_purple = camera()
+	while True:
+		frame, time, corners = camera()
 
-        if c_blue != (-1, -1) and c_purple != (-1, -1):
-            data['position'].append([time, path[target], c_blue, c_purple])
+		if len(corners):
+			data['position'].append([time, path[target], corners, (amplitude, direction)])
 
-        # calculate FPS
-        FPS.append(time)
-        if len(FPS) > 10:
-            FPS = FPS[-10:]
-            print(10 / (FPS[-1] - FPS[-10]))
+		# calculate FPS
+		"""
+		FPS.append(time)
+		if len(FPS) > 10:
+			FPS = FPS[-10:]
+			print(10 / (FPS[-1] - FPS[-10]))
+		"""
+		# high light target
+		target_size = 10
+		target_color = (40, 20, 220)
 
-        # calculate the position of robot as center of both circles
-        c = ((c_blue[0] + c_purple[0]) / 2,
-             (c_blue[1] + c_purple[1]) / 2)
+		cv2.line(frame, (path[target][0] - target_size, path[target][1]),
+				 (path[target][0] + target_size, path[target][1]),
+				 target_color, 2)
+		cv2.line(frame, (path[target][0], path[target][1] - target_size),
+				 (path[target][0], path[target][1] + target_size),
+				 target_color, 2)
 
-        # high light target
-        target_size = 10
-        target_color = (40, 20, 220)
+		# draw a line from current position to target
+		if corners:
+			corner = corners[0]
+			current_position = (int((corner[0, 0, 0] + corner[0, 1, 0] + corner[0, 2, 0] + corner[0, 3, 0]) / 4),
+								int((corner[0, 0, 1] + corner[0, 1, 1] + corner[0, 2, 1] + corner[0, 3, 1]) / 4))
 
-        cv2.line(frame, (path[target][0] - target_size, path[target][1]),
-                 (path[target][0] + target_size, path[target][1]),
-                 target_color, 2)
-        cv2.line(frame, (path[target][0], path[target][1] - target_size),
-                 (path[target][0], path[target][1] + target_size),
-                 target_color, 2)
+			cv2.line(frame, (path[target][0], path[target][1]),
+					 (int(current_position[0]), int(current_position[1])), (255, 255, 255), 2)
 
-        # draw a line from current position to target
-        if c != (-1, -1):
-            cv2.line(frame, (path[target][0], path[target][1]),
-                     (int(c[0]), int(c[1])), (255, 255, 255), 2)
+			# send date to control unite
 
-        # show frame
-        cv2.imshow('experiment', frame)
+			if (current_position[0] - path[target][0]) ** 2 + \
+				(current_position[1] - path[target][1]) ** 2 < reach_target_distance ** 2:
+				target += 1
 
-        # send date to control unite
+				# reached the last target
+				if target == len(path):
+					break
 
-        if (c[0] - path[target][0]) ** 2 + (c[1] - path[target][1]) ** 2 < reach_target_distance ** 2:
-            target += 1
+		# show frame
+		cv2.imshow('experiment', frame)
 
-            # reached the last target
-            if target == len(path):
-                break
+		key = cv2.waitKey(10) & 0xff
 
-        key = cv2.waitKey(20) & 0xff
-        if key == 27:
-            break
+		if key == 27:
+			break
+		else:
+			control(corners, path[target])
 
-    report()
-    cap.release()
-    cv2.destroyAllWindows()
+	report()
+	cap.release()
+	cv2.destroyAllWindows()
