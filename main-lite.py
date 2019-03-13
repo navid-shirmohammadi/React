e7c05312de2e9ba9296c0bca936c1e8ec3802087
navid-shirmohammadi <47769554+navid-shirmohammadi@users.noreply.github.com>
2019-03-13 14:30:53 +0330

    Update main-lite.py

--- main-lite.py
+++ main-lite.py
@@ -63,19 +63,8 @@ def camera():
 	""" this function handle all camera related operations on one frame and return frame and time """
 	if cap.isOpened():
 		_, frame = cap.read()  # read a frame
-		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-		cdf = cv2.equalizeHist(gray)
-		corners, ids, rejectedImgPoints = aruco.detectMarkers(cdf, aruco_dict, parameters=parameters)
-
-		for corner in corners:
-			coord1 = (int(corner[0, 0, 0]), int(corner[0, 0, 1]))
-			coord2 = (int(corner[0, 1, 0]), int(corner[0, 1, 1]))
-			coord3 = (int(corner[0, 2, 0]), int(corner[0, 2, 1]))
-			coord4 = (int(corner[0, 3, 0]), int(corner[0, 3, 1]))
-			cv2.line(frame, coord1, coord2, (0, 0, 255), 3)
-			cv2.line(frame, coord1, coord4, (255, 0, 0), 3)
-
-		return frame, timer() - start, corners
+		
+		return frame, timer() - start
 
 
 def report():
@@ -84,7 +73,7 @@ def report():
 		file.write('\n=========================\n')
 		file.write(str(data['start time']))
 		file.write('\n=========================\n')
-
+		
 		for item in data['position']:
 			file.write(str(item) + '\n')
 		file.close()
@@ -93,26 +82,24 @@ def report():
 angle_backup = 0
 
 
-def control(fish_corners, target_):
+def control(target_):
 	global ser, amplitude, direction, freq, bias_angle, angle_backup, frame, ROI_coord
 	
 	t = timer() - start
-
-	angle = angle_backup
-  pt0 = ROI_coord
+		
+	pt0 = ROI_coord
 	pt1 = target_
 	direction = (scipy.angle((pt0[0] - pt1[0]) + (pt0[1] - pt1[1]) * 1j, True) - 90) % 360
-
+	
 	angle = (direction + bias_angle * sin(freq * 2 * pi * t))
 	angle_backup = angle
 	
-
 	x = amplitude * cos(angle * pi / 180)
 	y = amplitude * sin(angle * pi / 180)
-
+	
 	poly_x = 114.8 * abs(x) ** 0.5157
 	poly_y = 114.8 * abs(y) ** 0.5157
-
+	
 	if x < 0:
 		poly_x = -poly_x
 	if y < 0:
@@ -120,10 +107,11 @@ def control(fish_corners, target_):
 	
 	scale = 1
 	
-	cv2.line(frame, (int(pt0[0]), int(pt0[1])), (int(pt0[0]+scale*poly_x), int(pt0[1]+scale*poly_y)), (0, 200, 100), 2)
-
+	cv2.line(frame, (int(pt0[0]), int(pt0[1])), (int(pt0[0] + scale * poly_x), int(pt0[1] + scale * poly_y)),
+			 (0, 200, 100), 2)
+	
 	COMMAND = str(int(poly_x)) + '^' + str(int(poly_y)) + '!'
-
+	
 	ser.write(bytes(COMMAND, 'ascii'))
 
 
@@ -135,48 +123,48 @@ def control(fish_corners, target_):
 
 if __name__ == '__main__':
 	# enter editor
-	frame, _, _ = camera()  # just get a frame for editor
+	frame, _ = camera()  # just get a frame for editor
 	
 	# ROI
 	bbox = cv2.selectROI(frame, False)
 	ok = tracker.init(frame, bbox)
 	
 	editor(frame)
-
+	
 	target = 0  # index of target robot has to reach
-
+	
 	while True:
-		frame, time, corners = camera()
-
+		frame, time = camera()
+		
 		ok, bbox = tracker.update(frame)
 		
 		# high light target
 		target_size = 10
 		target_color = (40, 20, 220)
-
+		
 		cv2.line(frame, (path[target][0] - target_size, path[target][1]),
 				 (path[target][0] + target_size, path[target][1]),
 				 target_color, 2)
 		cv2.line(frame, (path[target][0], path[target][1] - target_size),
 				 (path[target][0], path[target][1] + target_size),
 				 target_color, 2)
-
+		
 		# draw a line from current position to target
 		if ok:
 			p1 = (int(bbox[0]), int(bbox[1]))
 			p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
 			
+			ROI_coord = (int((p1[0] + p2[0]) / 2), int((p1[1] + p2[1]) / 2))
 			
-			ROI_coord = (int((p1[0]+p2[0])/2), int((p1[1]+p2[1])/2))
-			
-			data['position'].append(['ROI', time, path[target], (int(ROI_coord[0]), int(ROI_coord[1])), (amplitude, direction)])
+			data['position'].append(
+				['ROI', time, path[target], (int(ROI_coord[0]), int(ROI_coord[1])), (amplitude, direction)])
 			
 			cv2.rectangle(frame, p1, p2, (255, 0, 0), 2, 1)
 			
 			if (ROI_coord[0] - path[target][0]) ** 2 + \
 				(ROI_coord[1] - path[target][1]) ** 2 < reach_target_distance ** 2:
 				target += 1
-
+				
 				# reached the last target
 				if target == len(path):
 					break
@@ -184,17 +172,13 @@ if __name__ == '__main__':
 			cv2.putText(frame, "Tracking failure detected", (100, 80), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2)
 		cv2.putText(frame, tracker_type + " Tracker", (100, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50, 170, 50), 2)
 		
-		
-		
-		
-		
 		key = cv2.waitKey(10) & 0xff
-
+		
 		if key == 27:
 			break
 		else:
-			control(corners, path[target])
-			
+			control(path[target])
+		
 		# show frame
 		cv2.imshow('experiment', frame)
 	
